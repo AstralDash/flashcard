b4e24b3aea4e164edbaf6aab771510c2874e4e9d
AstralDash <ygoryanpedro008@gmail.com>
2024-11-26 21:30:16 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,18 +6,18 @@ criaCartao(
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é uma variavél?',
+    'Uma sequência de números que se variam'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é bye (BAI)'
 )
